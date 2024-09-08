db0609722dfe007a49e1baa7a04abc66f051838a
tantanishot <tristan.gabriel.santor@gmail.com>
2024-09-08 12:15:04 -0700

    changed api key

---
+++ src/app/api/link/route.js
@@ -0,0 +1,69 @@
+import { NextResponse } from 'next/server'
+import { Pinecone } from '@pinecone-database/pinecone'
+import puppeteer from 'puppeteer';
+
+export async function POST(req) {
+    console.log('Received POST -> link');
+    
+    if (req.method !== 'POST') {
+        return new NextResponse(JSON.stringify({ error: "Method not allowed" }), {
+            status: 405,
+            headers: { 'Content-Type': 'application/json' },
+        });
+    }
+
+    let browser;
+    try {
+        console.log('Initializing Pinecone');
+        const pc = new Pinecone({
+            apiKey: process.env.PINECONE_API_KEY,
+        });
+        const index = pc.index('rag').namespace('ns1');
+
+        const data = await req.json();
+        console.log('Request body:', data);
+
+        if (!Array.isArray(data) || data.length === 0) {
+            throw new Error('Invalid request body');
+        }
+
+        const link = data[data.length - 1].content;
+        console.log('Link:', link);
+
+        browser = await puppeteer.launch({ headless: true });
+        const page = await browser.newPage();
+       
+        await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36');
+       
+        await page.goto(link, { waitUntil: 'networkidle0' });
+
+        const rating = await page.evaluate(() => {
+          const ratingElement = document.querySelector('.RatingValue__Numerator-qw8sqy-2');
+          return ratingElement ? ratingElement.textContent.trim() : 'N/A';
+        });
+
+        const prof = await page.evaluate(() => {
+            const profElement = document.querySelector('.NameTitle__Name-dowf0z-0');
+            return profElement ? profElement.textContent.trim() : 'N/A';
+        });
+
+        return new NextResponse(JSON.stringify({ "status": "link accessed", "score": rating, "name": prof }), {
+            status: 200,
+            headers: { 'Content-Type': 'application/json' },
+        });
+    } catch (error) {
+        console.error('API route error:', error);
+        
+        let errorMessage = 'An unexpected error occurred';
+        let statusCode = 500;
+
+        return new NextResponse(JSON.stringify({ error: errorMessage }), {
+            status: statusCode,
+            headers: { 'Content-Type': 'application/json' },
+        });
+    } finally {
+        if (browser) {
+            await browser.close();
+        }
+    }
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,210 @@
+'use client'
+import { Box, Button, Stack, TextField } from '@mui/material'
+import { useEffect, useState, useRef } from 'react'
+
+function validUrl(message: string) {
+  let url;
+  try {
+    url = new URL(message);
+  } catch (_) {
+    return false;
+  }
+
+  return url.protocol === "http:" || url.protocol === "https:";
+}
+
+export default function Home() {
+  const [messages, setMessages] = useState([
+    {
+      role: 'assistant',
+      content: `Hi! I'm the Rate My Professor support assistant. How can I help you today?`,
+    },
+  ]);
+  const [message, setMessage] = useState('');
+
+  const bottomScroll = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (bottomScroll.current) {
+      bottomScroll.current.scrollIntoView({ behavior: 'smooth' });
+    }
+  }, [messages]);
+
+  const sendMessage = async () => {
+    setMessage('');
+    setMessages((messages) => [
+      ...messages,
+      { role: 'user', content: message },
+      { role: 'assistant', content: '' },
+    ]);
+
+    if (validUrl(message)) {
+      const response = await fetch('/api/link', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify([...messages, { role: 'user', content: message }]),
+      });
+
+      if (!response.ok) {
+        console.log("response not ok");
+      }
+
+      const data = await response.json();
+      const rating = data.score;
+      const prof = data.name;
+
+      const returnedResponse = await fetch('/api/chat', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify([
+          ...messages,
+          { role: 'user', content: message },
+          { role: 'system', content: `Professor ${prof} has a rating of ${rating}` }
+        ]),
+      }).then(async (res) => {
+        if (res.body) {
+          const reader = res.body.getReader();
+          const decoder = new TextDecoder();
+          let result = '';
+
+          return reader.read().then(function processText({ done, value }): Promise<string> {
+            return new Promise((resolve) => {
+              if (done) {
+                resolve(result);
+              } else {
+                const text = decoder.decode(value || new Uint8Array(), { stream: true });
+                setMessages((messages) => {
+                  let lastMessage = messages[messages.length - 1];
+                  let otherMessages = messages.slice(0, messages.length - 1);
+                  return [
+                    ...otherMessages,
+                    { ...lastMessage, content: lastMessage.content + text },
+                  ];
+                });
+                result += text;
+                resolve(result);
+                return reader.read().then(processText);
+              }
+            });
+          });
+        } else {
+          console.log("res body is null");
+        }
+      });
+
+      return;
+    } else {
+      const response = fetch('/api/chat', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify([...messages, { role: 'user', content: message }]),
+      }).then(async (res) => {
+        if (res.body) {
+          const reader = res.body.getReader();
+          const decoder = new TextDecoder();
+          let result = '';
+
+          return reader.read().then(function processText({ done, value }): Promise<string> {
+            return new Promise((resolve) => {
+              if (done) {
+                resolve(result);
+              } else {
+                const text = decoder.decode(value || new Uint8Array(), { stream: true });
+                setMessages((messages) => {
+                  let lastMessage = messages[messages.length - 1];
+                  let otherMessages = messages.slice(0, messages.length - 1);
+                  return [
+                    ...otherMessages,
+                    { ...lastMessage, content: lastMessage.content + text },
+                  ];
+                });
+                result += text;
+                resolve(result);
+                return reader.read().then(processText);
+              }
+            });
+          });
+        }
+      });
+    }
+  };
+
+  return (
+    <Box
+      width="100vw"
+      height="100vh"
+      display="flex"
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Stack
+        direction={'column'}
+        width="500px"
+        height="700px"
+        borderRadius="16px"
+        boxShadow="0 0 150px skyBlue"
+        p={2}
+        spacing={3}
+      >
+        <Stack
+          direction={'column'}
+          spacing={2}
+          flexGrow={1}
+          overflow="auto"
+          maxHeight="100%"
+        >
+          {messages.map((message, index) => (
+            <Box
+              key={index}
+              display="flex"
+              justifyContent={
+                message.role === 'assistant' ? 'flex-start' : 'flex-end'
+              }
+            >
+              <Box
+                maxWidth="75%"
+                bgcolor={
+                  message.role === 'assistant'
+                    ? 'primary.light'
+                    : 'error.light'
+                }
+                borderRadius={
+                  message.role === 'assistant' ? "16px 16px 16px 0px" : "16px 16px 0px 16px"
+                }
+                color="white"
+                p={3}
+              >
+                {message.content}
+              </Box>
+            </Box>
+          ))}
+          <div ref={bottomScroll}></div>
+        </Stack>
+        <Stack direction={'row'} spacing={2}>
+          <TextField
+            label="Message"
+            fullWidth
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            onKeyDown={(ev) => {
+              if (ev.key === 'Enter') {
+                sendMessage();
+                ev.preventDefault();
+              }
+            }}
+          />
+          <Button variant="contained" onClick={sendMessage}>
+            Send
+          </Button>
+        </Stack>
+      </Stack>
+    </Box>
+  );
+}
